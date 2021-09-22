8b198fa35fb3985b642cebbec3eee2cf8b3f7b9b
Christiani Igot <c.bensi.igot@gmail.com>
2021-09-22 18:56:44 -0400

    first commit

---
+++ validation.py
@@ -0,0 +1,22 @@
+import re
+
+def validate(form_data,checks,form_name="form",strict=True,func=print):
+	valid = True
+	for key, val in checks.items():
+		if key in form_data:
+			for check in val:
+				if "lambda" in check:
+					if not check["lambda"](form_data[key]):
+						valid = False
+						func(check["error"],check["category"])
+				if type(form_data[key]) == str and "regex" in check:
+					regex = re.compile(check["regex"])
+					if not regex.match(form_data[key]):
+						valid = False
+						func(check["error"],check["category"])
+					
+		elif strict and key not in form_data:
+			valid = False
+			func(f"Field {key} not submitted",form_name+"_messages")
+	
+	return valid
