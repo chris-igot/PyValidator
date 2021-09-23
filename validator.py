305ba7c729b4597e66884979a1e036c0d87a02b6
Christiani Igot <c.bensi.igot@gmail.com>
2021-09-23 19:08:13 -0400

    Added example

--- validator.py
+++ validator.py
@@ -9,11 +9,15 @@ def validate(form_data,checks,form_name="form",strict=True,func=print):
 					if not check["lambda"](form_data[key]):
 						valid = False
 						func(check["error"],check["category"])
+						if "showstopper" in check and check["showstopper"]:
+							break
 				if type(form_data[key]) == str and "regex" in check:
 					regex = re.compile(check["regex"])
 					if not regex.match(form_data[key]):
 						valid = False
 						func(check["error"],check["category"])
+						if "showstopper" in check and check["showstopper"]:
+							break
 					
 		elif strict and key not in form_data:
 			valid = False
